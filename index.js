0c231499c961dff4720528be71c188be4d86c4cf
Иван Балашов <balashov@MacBook-Pro-Ivan.local>
2020-10-15 01:59:14 +0300

    glitch autoupdate

--- index.js
+++ index.js
@@ -1230,6 +1230,14 @@ function router (req, res, requestedFile) {
     }
   }
   loadFile(req, res, requestedFile);
+  if (req.headers['x-github-event'] == "push") {
+    cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+    cmd.get('./git.sh', (err, data) => {  // Run our script
+      if (data) console.log(data);
+      if (err) console.log(err);
+    });
+    cmd.run('refresh');  // Refresh project
+  }
 }
 
 function getError(req, res, requestedFile) {
@@ -1326,13 +1334,13 @@ app.post('/git', (req, res) => {
     // If event is "push"
     if (req.headers['x-github-event'] == "push") {
       cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
-    cmd.get('./git.sh', (err, data) => {  // Run our script
-      if (data) console.log(data);
-      if (err) console.log(err);
-    });
-    cmd.run('refresh');  // Refresh project
+      cmd.get('./git.sh', (err, data) => {  // Run our script
+        if (data) console.log(data);
+        if (err) console.log(err);
+      });
+      cmd.run('refresh');  // Refresh project
 
-    console.log("> [GIT] Updated with origin/master");
+      console.log("> [GIT] Updated with origin/master");
     }
 
     return res.sendStatus(200); // Send back OK status
