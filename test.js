cbcbd2805569add21673dc8ad136ca26bef2dea3
Иван Балашов <balashov@MacBook-Pro-Ivan.local>
2020-10-15 01:20:59 +0300

    test

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-function search(value) {
-  let data = 'first'
-
-  if (data.search(value) != -1 ) {
-    console.log(value);
-  } else {
-    console.log('dont match');
-  }
-}
-
-search("tr");
+//test
