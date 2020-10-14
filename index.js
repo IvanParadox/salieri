a43e033fff8ac81888a4d0aecb317d87d897cdae
Иван Балашов <balashov@MacBook-Pro-Ivan.local>
2020-10-15 00:59:11 +0300

    for glitch autoupdate

--- index.js
+++ index.js
@@ -1348,3 +1348,21 @@ server.listen(port, (err) => {
         return console.log('something bad happened', err);
     }    console.log(`server is listening on ${port}`);
 });
+
+const cmd = require("node-cmd");
+
+app.post('/git', (req, res) => {
+  // If event is "push"
+  if (req.headers['x-github-event'] == "push") {
+    cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+  cmd.get('./git.sh', (err, data) => {  // Run our script
+    if (data) console.log(data);
+    if (err) console.log(err);
+  });
+  cmd.run('refresh');  // Refresh project
+
+  console.log("> [GIT] Updated with origin/master");
+  }
+
+  return res.sendStatus(200); // Send back OK status
+});
